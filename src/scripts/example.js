23e0937a0ebe16be9a71273b310c6722e96cc2f7
Shane Garner <shane.garner@genesys.com>
2022-07-13 16:19:17 -0500

    got click to email with attachment working

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,6 +1,18 @@
+const platformClient = require('platformClient');
+
 document.addEventListener('DOMContentLoaded',function(){
-    
+    const client = platformClient.ApiClient.instance;
+    let loginData;
+    client.loginImplicitGrant("31e8192e-21e1-4f5c-9463-f4567fc83373", "https://localhost/example.html")
+        .then((data) => {
+            loginData = data;
+        })
+        .catch((err) => {
+            // Handle failure response
+            console.log(err);
+        });
     document.getElementById("clickToDial").addEventListener("click", clickToDial);
+    document.getElementById("clickToEmail").addEventListener("click", clickToEmail);
     document.getElementById("addAssociation").addEventListener("click", addAssociation);
     document.getElementById("addAttribute").addEventListener("click", addAttribute);
     document.getElementById('addTransferContext').addEventListener("click", addTransferContext);
@@ -12,7 +24,7 @@ document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('muteInteraction').addEventListener("click", updateInteractionState);
     document.getElementById('updateAudioConfiguration').addEventListener("click", updateAudioConfiguration);
     document.getElementById('sendCustomNotification').addEventListener("click", sendCustomNotification);
-    
+
     document.getElementById('view-interactionList').addEventListener("click", setView);
     document.getElementById('view-calllog').addEventListener("click", setView);
     document.getElementById('view-newInteraction').addEventListener("click", setView);
@@ -48,7 +60,52 @@ document.addEventListener('DOMContentLoaded',function(){
             data: { number: '3172222222', autoPlace: true }
         }), "*");
     }
+    function clickToEmail() {
+        console.log('process click to email');
+        // document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
+        //     type: 'clickToDial',
+        //     data: { address: 'shane.garner@genesys.com', type: 'email', autoPlace: true }
+        // }), "*");
 
+        let conversationApiInstance = new platformClient.ConversationsApi();
+                const body = {
+                    "queueId": "60e328d1-60ed-43e4-8d5c-2286dad98b68",
+                    "toAddress": "shane@garnercrew.com",
+                    "toName": "Shane Garner",
+                    "direction": "OUTBOUND",
+                    "textBody": "Hey now again from EF",
+                    "subject": "Api Email EF"
+                };
+                conversationApiInstance.postConversationsEmails(body)
+                    .then((emailRespData) => {
+                        console.log(`postConversationsEmails success! data: ${JSON.stringify(emailRespData, null, 2)}`);
+                        fetch('https://localhost/assets/Genesys.pdf')
+                            .then(res => res.blob())
+                            .then(blob => {
+                                console.log("we have the blob!");
+                                const fd = new FormData();
+                                fd.append('conversationId', emailRespData.id);
+                                fd.append('file', new File([blob], 'Genesys.pdf'));
+                                axios.post('https://apps.mypurecloud.com/uploads/postino-attachments', fd,
+                                    {
+                                        headers: {
+                                            'Authorization': 'Bearer ' + loginData.accessToken,
+                                            'Content-Type': 'multipart/form-data'
+                                        }
+                                    })
+                                    .then(res => console.log("done uploading attachment" + JSON.stringify(res)))
+                                    .catch((err) => {
+                                        // Handle failure response
+                                        console.log("error with axios", err);
+                                    });
+                            });
+                    })
+                    .catch((err) => {
+                        console.log('There was a failure calling postConversationsEmails');
+                        console.error(err);
+                    });
+
+    }
     function addAssociation() {
         console.log('process add association');
         document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
@@ -128,7 +185,7 @@ document.addEventListener('DOMContentLoaded',function(){
     function setView(event) {
         console.log('process view update');
         let payload = {
-            type:"main", 
+            type:"main",
             view: {
                 name:event.target.outerText
             }
@@ -143,7 +200,7 @@ document.addEventListener('DOMContentLoaded',function(){
         console.log('Send Custom User Notification');
         var payload = {
             message: document.getElementById('customNotificationMessage').value,
-            type: document.getElementById('notificationType').value,  
+            type: document.getElementById('notificationType').value,
             timeout: document.getElementById('notificationTimeout').value
         };
         document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
